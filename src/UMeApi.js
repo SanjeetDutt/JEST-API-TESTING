ab4f23715b32660c632acb81add9d7ad93fcc6f8
Sanjeet Dutt <sanjeet@solvative.com>
2023-10-11 21:00:36 +0530

    Update the reporting message to add all parameters in the reposr

--- src/UMeApi.js
+++ src/UMeApi.js
@@ -7,163 +7,174 @@ const umeCommonQuery = ["collation=BREADTH_FIRST_ORDER", "embed=true"]
 
 export default class UMeApi extends Api {
 
-  constructor(csvRow) {
-    super()
-    this.UMe = new UMe(csvRow, csvRow[_Config.ResultColumnName])
-  }
-
-  async startEnquiry(reporting) {
-    const url = super.buildURL([_Config.muleSoftHostAddress, _Config.umeProxy, "startEnquiry"], umeBranchQuery)
-    const headers = super.buildJsonHeader()
-    const startEnquiryAnswer = this.UMe.getStartEnquiryAnswer()
-
-    const body = {
-      answers: {
-        ...startEnquiryAnswer
-      }, username: _Config.umeUsername
+    constructor(csvRow) {
+        super()
+        this.UMe = new UMe(csvRow, csvRow[_Config.ResultColumnName])
     }
 
-    reporting.addRequest(body)
+    async startEnquiry(reporting) {
+        const url = super.buildURL([_Config.muleSoftHostAddress, _Config.umeProxy, "startEnquiry"], umeBranchQuery)
+        const headers = super.buildJsonHeader()
+        const startEnquiryAnswer = this.UMe.getStartEnquiryAnswer()
 
-    try {
-      const response = await super.post({ url, headers, body })
-      this.UMe.setEnquiryId(response.enquiryId)
-      reporting.addResponse(response)
-      reporting.addReport("enquiry id", response.enquiryId)
-      return STATUS.OK
-    } catch (e) {
-      console.error("ENCOUNTER AN ERROR WHILE STARTING AN ENQUIRY", e);
-      return STATUS.ERROR
+        const body = {
+            answers: {
+                ...startEnquiryAnswer
+            }, username: _Config.umeUsername
+        }
+
+        reporting.addRequest(body)
+
+        try {
+            const response = await super.post({ url, headers, body })
+            this.UMe.setEnquiryId(response.enquiryId)
+            reporting.addResponse(response)
+            reporting.addReport("enquiry id", response.enquiryId)
+            return STATUS.OK
+        } catch (e) {
+            console.error("ENCOUNTER AN ERROR WHILE STARTING AN ENQUIRY", e);
+            return STATUS.ERROR
+        }
+
     }
 
-  }
-
-  seekValidationError(reporting, response, successMessage) {
-    const error = []
-    response.sections.forEach(section => {
-      section.enquiryLines.forEach(enquiryLine => {
-        enquiryLine.questions.forEach(question => {
-          const errorObj = question.validationErrors
-          Object.values(errorObj).forEach(err => {
-            error.push(err)
-          })
+    seekValidationError(reporting, response, successMessage) {
+        const error = []
+        response.sections.forEach(section => {
+            section.enquiryLines.forEach(enquiryLine => {
+                enquiryLine.questions.forEach(question => {
+                    const errorObj = question.validationErrors
+                    Object.values(errorObj).forEach(err => {
+                        error.push(err)
+                    })
+                })
+            })
         })
-      })
-    })
 
-    if (error.length > 0) {
-      reporting.addReport("error", error)
-      return true
-    } else {
-      reporting.addReport("message", successMessage)
-      return false
-    }
-  }
-
-  async postEnquiry(reporting) {
-    const enquiryId = this.UMe.enquiryId
-    const url = super.buildURL([_Config.muleSoftHostAddress, _Config.umeProxy, "enquiry", enquiryId], umeCommonQuery)
-    const headers = super.buildJsonHeader()
-    const answers = this.UMe.getEnquiryAnswer()
-    const body = {
-      answers: {
-        ...answers
-      },
-      locale: _Config.umeLocale,
-      username: _Config.umeUsername,
-      debug: true
-    }
-    reporting.addRequest(body)
-
-    try {
-      const r = await super.post({ url, headers, body })
-      reporting.addResponse(r)
-      const error = this.seekValidationError(reporting, r, "Enquiry posted successfully")
-      return !!error ? STATUS.ERROR : STATUS.OK
-    } catch (e) {
-      console.error("ENCOUNTER AN ERROR WHILE POSING AN ENQUIRY", e);
-      return STATUS.ERROR
+        if (error.length > 0) {
+            reporting.addReport("error", error)
+            return true
+        } else {
+            reporting.addReport("message", successMessage)
+            return false
+        }
     }
 
-  }
+    async postEnquiry(reporting) {
+        const enquiryId = this.UMe.enquiryId
+        const url = super.buildURL([_Config.muleSoftHostAddress, _Config.umeProxy, "enquiry", enquiryId], umeCommonQuery)
+        const headers = super.buildJsonHeader()
+        const answers = this.UMe.getEnquiryAnswer()
+        const body = {
+            answers: {
+                ...answers
+            },
+            locale: _Config.umeLocale,
+            username: _Config.umeUsername,
+            debug: true
+        }
+        reporting.addRequest(body)
 
-  async closeEnquiry(reporting) {
-    const enquiryId = this.UMe.enquiryId
-    const url = super.buildURL([_Config.muleSoftHostAddress, _Config.umeProxy, "closeEnquiry", enquiryId], umeCommonQuery)
-    const headers = super.buildJsonHeader()
+        try {
+            const r = await super.post({ url, headers, body })
+            reporting.addResponse(r)
+            const error = this.seekValidationError(reporting, r, "Enquiry posted successfully")
+            return !!error ? STATUS.ERROR : STATUS.OK
+        } catch (e) {
+            console.error("ENCOUNTER AN ERROR WHILE POSING AN ENQUIRY", e);
+            return STATUS.ERROR
+        }
 
-    reporting.addRequest({})
-    try {
-      const r = await super.post({ url, headers })
-      reporting.addResponse(r)
-      const error = this.seekValidationError(reporting, r, "Enquiry closed successfully")
-      return !!error ? STATUS.ERROR : STATUS.OK
-    } catch (e) {
-      console.error("ENCOUNTER AN ERROR WHILE CLOSING AN ENQUIRY", e);
-      return STATUS.ERROR
-    }
-  }
-
-  parseBucketToReport(buckets) {
-    const result = {}
-
-    buckets.forEach(bucket => {
-      result[`${bucket.name} : ${bucket.max.value}`] = bucket.contributions.map(contribution => {
-        return `${contribution.sources[0]} : ${contribution.value}`
-      })
-
-    })
-
-    return result
-  }
-
-  async enquiryResult(reporting) {
-
-    const enquiryId = this.UMe.enquiryId
-    const url = super.buildURL([_Config.muleSoftHostAddress, _Config.umeProxy, "enquiry/history", enquiryId], umeCommonQuery)
-    const headers = super.buildJsonHeader()
-    reporting.addRequest({})
-
-    try {
-      const r = await super.get({ url, headers })
-      reporting.addResponse(r)
-      const buckets = r.snapshots[0].enquiry.buckets
-      reporting.addReport("Bucket Outcome", this.parseBucketToReport(buckets))
-      const resultArray = this.UMe.generateEnquiryResultArray(buckets)
-      // reporting.addReport("Evaluation Result", this.parseEvaluationResult(resultArray))
-      return resultArray
-
-    } catch (e) {
-      console.error("ENCOUNTER AN ERROR WHILE GETTING ENQUIRY HISTORY", e);
-      return STATUS.ERROR
     }
 
-  }
+    async closeEnquiry(reporting) {
+        const enquiryId = this.UMe.enquiryId
+        const url = super.buildURL([_Config.muleSoftHostAddress, _Config.umeProxy, "closeEnquiry", enquiryId], umeCommonQuery)
+        const headers = super.buildJsonHeader()
 
-  parseEvaluationResult(resultArray) {
-    return resultArray.map(r => `${r.bucketName}->${r.sourceName}:[Expecting:${r.expectation}][GETTING:${r.value}]`)
-  }
+        reporting.addRequest({})
+        try {
+            const r = await super.post({ url, headers })
+            reporting.addResponse(r)
+            const error = this.seekValidationError(reporting, r, "Enquiry closed successfully")
+            return !!error ? STATUS.ERROR : STATUS.OK
+        } catch (e) {
+            console.error("ENCOUNTER AN ERROR WHILE CLOSING AN ENQUIRY", e);
+            return STATUS.ERROR
+        }
+    }
 
-  async completeEnquiry(reporting) {
+    parseBucketToReport(buckets) {
+        const result = {}
 
-    const startEnquiry = await this.startEnquiry(reporting)
-    console.log("Start enquiry : " + startEnquiry);
+        const bucketPrint = (bucket) => {
+            const values = []
+            Object.keys(bucket).forEach(key => {
+                if (["contributions", "type", "name"].includes(key))
+                    return
+                values.push(`${key}=${bucket[key].value}`)
+            })
 
-    const postEnquiry = await this.postEnquiry(reporting)
-    console.log("Post enquiry : " + postEnquiry);
+            return values.join(", ")
+        }
 
-    const closeEnquiry = await this.closeEnquiry(reporting)
-    console.log("Close enquiry : " + closeEnquiry);
+        buckets.forEach(bucket => {
+            result[`${bucket.name} : ${bucketPrint(bucket)}`] = bucket.contributions.map(contribution => {
+                return `${contribution.sources[0]} : ${contribution.value}`
+            })
 
-    const enquiryResult = await this.enquiryResult(reporting)
-    console.log("Enquiry result : " + enquiryResult);
+        })
 
-    return (
-      startEnquiry === STATUS.OK &&
-      postEnquiry === STATUS.OK &&
-      closeEnquiry === STATUS.OK &&
-      enquiryResult === STATUS.OK
-    ) ? STATUS.OK : STATUS.ERROR
+        return result
+    }
 
-  }
+    async enquiryResult(reporting) {
+
+        const enquiryId = this.UMe.enquiryId
+        const url = super.buildURL([_Config.muleSoftHostAddress, _Config.umeProxy, "enquiry/history", enquiryId], umeCommonQuery)
+        const headers = super.buildJsonHeader()
+        reporting.addRequest({})
+
+        try {
+            const r = await super.get({ url, headers })
+            reporting.addResponse(r)
+            const buckets = r.snapshots[0].enquiry.buckets
+            reporting.addReport("Bucket Outcome", this.parseBucketToReport(buckets))
+            const resultArray = this.UMe.generateEnquiryResultArray(buckets)
+            // reporting.addReport("Evaluation Result", this.parseEvaluationResult(resultArray))
+            return resultArray
+
+        } catch (e) {
+            console.error("ENCOUNTER AN ERROR WHILE GETTING ENQUIRY HISTORY", e);
+            return STATUS.ERROR
+        }
+
+    }
+
+    parseEvaluationResult(resultArray) {
+        return resultArray.map(r => `${r.bucketName}->${r.sourceName}:[Expecting:${r.expectation}][GETTING:${r.value}]`)
+    }
+
+    async completeEnquiry(reporting) {
+
+        const startEnquiry = await this.startEnquiry(reporting)
+        console.log("Start enquiry : " + startEnquiry);
+
+        const postEnquiry = await this.postEnquiry(reporting)
+        console.log("Post enquiry : " + postEnquiry);
+
+        const closeEnquiry = await this.closeEnquiry(reporting)
+        console.log("Close enquiry : " + closeEnquiry);
+
+        const enquiryResult = await this.enquiryResult(reporting)
+        console.log("Enquiry result : " + enquiryResult);
+
+        return (
+            startEnquiry === STATUS.OK &&
+            postEnquiry === STATUS.OK &&
+            closeEnquiry === STATUS.OK &&
+            enquiryResult === STATUS.OK
+        ) ? STATUS.OK : STATUS.ERROR
+
+    }
 }
